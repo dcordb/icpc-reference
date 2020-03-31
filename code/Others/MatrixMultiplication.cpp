ec965e326aee3812d9a7adb0e682901e24177579
Daniel Enrique <dcordb97@gmail.com>
2020-03-31 18:16:32 -0400

    changed matrix multiplication to use vectors

--- code/Others/MatrixMultiplication.cpp
+++ code/Others/MatrixMultiplication.cpp
@@ -1,49 +1,42 @@
-//matrix multiplication con memoria dinamica, funciona ok
+const int MOD = 998244353;
+typedef vector <vector <int> > matrix;
 
-const int MOD = 1e9 + 7;
+matrix operator * (const matrix &a, const matrix &b) {
+	assert(a[0].size() == b.size());
 
-struct matrix { //matrix empieza de 0
-	int n, m;
-	int** a; //memoria dinamica
+	int n = a.size();
+	int m = b.size();
+	int p = b[0].size();
 
-	matrix() {}
+	matrix c(n, vector <int> (p));
 
-	matrix(int n_, int m_) {
-		n = n_, m = m_;
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < p; j++)
+			for(int k = 0; k < m; k++)
+				c[i][j] = (c[i][j] + 1LL * a[i][k] * b[k][j] % MOD) % MOD;
 
-		a = new int*[n];
+	return c;
+}
 
-		for(int i = 0; i < n; i++)
-			a[i] = new int[m];
+matrix I(int n) {
+	matrix a(n, vector <int> (n));
 
-		for(int i = 0; i < n; i++)
-			for(int j = 0; j < m; j++)
-				a[i][j] = 0;
+	for(int i = 0; i < n; i++)
+		a[i][i] = 1;
+
+	return a;
+}
+
+matrix exp(matrix a, int64 n) {
+	matrix ans = I(a.size());
+
+	while(n > 0) {
+		if(n % 2LL == 1LL)
+			ans = ans * a;
+			
+		a = a * a;
+		n /= 2LL;
 	}
-
-	matrix operator * (const matrix &t) {
-		int p = t.m;
-		matrix res(n, p);
-
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < p; j++) {
-				for(int k = 0; k < m; k++) {
-					int v = (a[i][k] * 1LL * t.a[k][j]) % MOD;
-					res.a[i][j] = (res.a[i][j] + v) % MOD;
-				}
-			}
-		}
-
-		return res;
-	}
-
-	void print() {
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < m; j++)
-				printf("%d ", a[i][j]);
-			printf("\n");
-		}
-
-		printf("\n");
-	}
-};
+	
+	return ans;
+}
