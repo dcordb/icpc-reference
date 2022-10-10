044ac924184c05f9730c4d118dd5fa2f924c685f
Daniel Enrique <dcordb97@gmail.com>
2022-10-10 15:44:44 -0400

    Add formatting.

--- code/NumberTheory/IterativeFastExponentiation.cpp
+++ code/NumberTheory/IterativeFastExponentiation.cpp
@@ -1,13 +1,12 @@
 int exp(int a, int n) {
-	int ans = 1;
+    int ans = 1;
 
-	while(n > 0) {
-		if(n % 2)
-			ans = 1LL * ans * a % MOD;
-			
-		a = 1LL * a * a % MOD;
-		n /= 2;
-	}
-	
-	return ans;
+    while (n > 0) {
+        if (n % 2) ans = 1LL * ans * a % MOD;
+
+        a = 1LL * a * a % MOD;
+        n /= 2;
+    }
+
+    return ans;
 }
